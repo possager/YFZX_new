9afbf45a595eeecd86e0c07c8bbb333cdc310b9f
passager <passager@163.com>
2017-08-16 00:55:50 +0800

    8-7

--- spiders/thepaper.py
+++ spiders/thepaper.py
@@ -13,6 +13,7 @@ from saveresult import Save_result
 import re
 import logging
 from visit_page import get_response_and_text
+from datetime import datetime,timedelta
 
 class thepaper:
     def __init__(self):
@@ -20,8 +21,8 @@ class thepaper:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
         }
         self.urls = [
-            'http://m.thepaper.cn/channel_25950',#时事
-            'http://m.thepaper.cn/channel_26916',#视频
+            'http://m.thepaper.cn/channel_26916',#视频，单独搞，因为视频模块的网页码不一样
+            'http://m.thepaper.cn/channel_25950',  # 时事
             'http://m.thepaper.cn/channel_25951',#财经
             'http://m.thepaper.cn/channel_25952',#思想
             'http://m.thepaper.cn/channel_25953',#生活
@@ -52,35 +53,108 @@ class thepaper:
             datare = Re_pattern.findall(response_in_function_text)
 
             url_in_content = datare[0].split('"')[1]
-            nexturl = 'http://m.thepaper.cn/load_channel.jsp?' + url_in_content#发现手机端的数据获得地更多一些
+            if 'http://m.thepaper.cn/channel_26916' in url:
+                nexturl = 'http://www.thepaper.cn/load_index.jsp?' + url_in_content#发现手机端的数据获得地更多一些,电脑端http://m.thepaper.cn/load_channel.jsp?
             # nexturl='http://www.thepaper.cn/load_index.jsp?nodeids='+url_in_content
             # 'http://www.thepaper.cn/load_index.jsp?nodeids='桌面版的
+            else:
+                nexturl='http://m.thepaper.cn/load_channel.jsp?'+url_in_content
 
             thisurls_list.append(nexturl)
         for url_to_visit in thisurls_list:
             for i in range(50):
-                # self.index_url_list.append(url_to_visit+str(i))
-                pass
-
-        # threadlist=[]
-        # while self.global_status_num_index > 0 or self.content_data_list:  # 如果index中的任务完了,content_url_list中是空的的时候，就停止
-        #     while self.content_data_list or threadlist:
-        #         for threadi in threadlist:
-        #             if not threadi.is_alive():
-        #                 threadlist.remove(threadi)
-        #         while len(threadlist) < CONTENT_THREADING_NUM and self.content_data_list:
-        #             data_in_while = self.content_data_list.pop()
-        #             thread_in_while = threading.Thread(target=get_content_inside, args=(data_in_while,))
-        #             thread_in_while.setDaemon(True)
-        #             thread_in_while.start()
-        #             threadlist.append(thread_in_while)
-        #             print 'len of threadlist in content---', len(threadlist)
-        #             print 'len of content_data_list in content is ---', len(self.content_data_list)
-        #             # time.sleep(1)
-        #
-        # self.global_status_num_content = 0
+                self.index_url_list.append(url_to_visit+str(i))
 
 
+        def get_index_inside_movie(url):
+            response2=get_response_and_text(url=url)
+            response_in_function=response2['response_in_function']
+            response_in_function_text=response2['response_in_function_text']
+            datasoup=BeautifulSoup(response_in_function_text,'lxml')
+            for one_url in datasoup.select('body > div'):
+                thisurl=one_url.select('h2 > a')[0].get('href')
+                publish_user=one_url.select('a')[2].text
+                title=one_url.select('a')[1].text
+                try:
+                    publish_time=one_url.select('a > span')[0].text
+                except:
+                    publish_time='00:00:00'#这些网页的格式不规则
+                try:
+                    publish_time_date=one_url.select('span')[1].text
+                    if u'天前' in publish_time_date:
+                        publish_time_date=publish_time_date.replace(u'天前','')
+                        # time_elapsed=int(publish_time_date)*24*60*60
+                        # time_now=time.time()
+                        # publish_time_date=
+                        date_now=datetime.now()
+                        date_now2=date_now-timedelta(days=int(publish_time_date))
+                        publish_time_date=date_now2
+                        # timearray=time.localtime()
+                        # publish_time_date=str(time.strftime('%Y-%m-%d %H-%M',publish_time_date))
+                        publish_time_date=str(publish_time_date.strftime('%Y-%m-%d %H-%M'))
+                except Exception as e:
+                    print e
+                    try:
+                        if len(one_url.select('span')[0].text)==10:
+                            publish_time_date=one_url.select('span')[0].text
+                        else:
+                            continue
+                    except:
+                        continue
+                publish_time=publish_time_date+' '+publish_time+':00'
+                id=one_url.select('h2 > a')[0].get('id')
+                try:
+                    reply_count= one_url.select('span.trbszan')[0].text
+                    if 'k' in reply_count:
+                        reply_count=reply_count.replace('.','').replace('k','00')
+                except:
+                    reply_count= 0
+
+                data_index={
+                    'url':'http://m.thepaper.cn/'+thisurl,
+                    'publish_user':publish_user,
+                    'title':title,
+                    'publish_time':publish_time,
+                    'id':id,
+                    'reply_count':reply_count
+                }
+                self.content_data_list.append(data_index)
+
+        def get_index_inside(url):
+            print url
+
+        threadlist=[]
+        while self.global_status_num_index > 0 or self.index_url_list:  # 如果index中的任务完了,content_url_list中是空的的时候，就停止
+            while self.index_url_list or threadlist:
+                for threadi in threadlist:
+                    if not threadi.is_alive():
+                        threadlist.remove(threadi)
+                while len(threadlist) < CONTENT_THREADING_NUM and self.index_url_list:
+                    data_in_while = self.index_url_list.pop()
+                    if 'http://www.thepaper.cn/load_index.jsp?' in data_in_while:
+                        thread_in_while = threading.Thread(target=get_index_inside_movie, args=(data_in_while,))
+                    else:
+                        thread_in_while=threading.Thread(target=get_index_inside,args=(data_in_while,))
+                    thread_in_while.setDaemon(True)
+                    thread_in_while.start()
+                    threadlist.append(thread_in_while)
+
+
+    def get_content(self):
+        pass
+
+    def run(self):
+        thread1 = threading.Thread(target=self.get_Index, args=())
+        thread1.start()
+        time.sleep(20)
+        thread2 = threading.Thread(target=self.get_content, args=())
+        thread2.start()
+        # time.sleep(3)
+        thread3 = threading.Thread(target=self.get_comments, args=())
+        thread3.start()
+        thread4 = threading.Thread(target=self.save_result, args=())
+        thread4.start()
+        pass
 
 if __name__ == '__main__':
     thisclass=thepaper()
--- testthepaper.py
+++ testthepaper.py
@@ -6,7 +6,7 @@ user_agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like
 user_agent2='Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
 
 headers={
-    'User-Agent':user_agent2
+    'User-Agent':user_agent
 }
 # response1=requests.get(url='http://m.thepaper.cn/channel_25950',headers=headers)
 # # print response1.text
@@ -17,21 +17,53 @@ headers={
 # nexturl='http://m.thepaper.cn/load_channel.jsp?'+url_in_content+str(1)
 # print nexturl
 
-url1='http://www.thepaper.cn/load_index.jsp?nodeids=nodeids=25448,26609,25942,26015,25599,25842,26862,25769,25990,26173,26202,26404,26490,&topCids=1762084,1759534&pageidx=0'
-url2='http://www.thepaper.cn/load_index.jsp?nodeids=nodeids=25444,27224,26525,26878,25483,25457,25574,25455,26937,25450,25482,25445,25456,25446,25536,26506,&topCids=1763063&pageidx=0'
-url3='http://www.thepaper.cn/load_index.jsp?nodeids=nodeids=25434,25436,25433,25438,25435,25437,27234,25485,25432,&topCids=1762842,1762790,1763110&pageidx=0'
-url4='http://www.thepaper.cn/load_index.jsp?nodeids=nodeids=26912,26918,26919,26965,26906,26907,26908,27260,26909,26910,26911,26913,26914,26915,&topCids=1763003,1763173,1762843&pageidx=0'
-url5='http://www.thepaper.cn/load_index.jsp?nodeids=nodeids=25462,25488,25489,25490,25423,25426,25424,25463,25491,25428,25464,25425,25429,25481,25430,25678,25427,25422,25487,25634,25635,25600,&topCids=1763004,1763242,1763002&pageidx=0'
+url1='http://m.thepaper.cn/load_channel.jsp?nodeids=nodeids=25448,26609,25942,26015,25599,25842,26862,25769,25990,26173,26202,26404,26490,&topCids=1762084,1759534&pageidx=0'
+url2='http://m.thepaper.cn/load_channel.jsp?nodeids=nodeids=25444,27224,26525,26878,25483,25457,25574,25455,26937,25450,25482,25445,25456,25446,25536,26506,&topCids=1763063&pageidx=0'
+url3='http://m.thepaper.cn/load_channel.jsp?nodeids=nodeids=25434,25436,25433,25438,25435,25437,27234,25485,25432,&topCids=1762842,1762790,1763110&pageidx=0'
+url4='http://www.thepaper.cn/load_index.jsp?nodeids=nodeids=26912,26918,26919,26965,26906,26907,26908,27260,26909,26910,26911,26913,26914,26915,&topCids=1763003,1763173,1762843&pageidx=18'
+url5='http://m.thepaper.cn/load_channel.jsp?nodeids=nodeids=25462,25488,25489,25490,25423,25426,25424,25463,25491,25428,25464,25425,25429,25481,25430,25678,25427,25422,25487,25634,25635,25600,&topCids=1763004,1763242,1763002&pageidx=0'
 
 
-response1=requests.get(url=url4,headers=headers)
+response1=requests.get(url=url5,headers=headers)
 # print response1.text
 datasoup=BeautifulSoup(response1.text,'lxml')
-print str(datasoup)
+# print str(datasoup)
 print '------------------------------------------------------------------------------------------------------'
-for urlmark1 in  datasoup.select('body > div'):
-    # print urlmark1.get('href')#href
-    print urlmark1.select('h2 > a')[0].get('href')#url
-    print urlmark1.select('p')[0].text#abstract
-    print urlmark1.select('a')[2].text
-    print urlmark1.select('a')[1].text
+# for urlmark1 in  datasoup.select('body > div'):
+#     # print urlmark1.get('href')#href
+#     print urlmark1.select('h2 > a')[0].get('href')#url
+#     print urlmark1.select('p')[0].text#abstract
+#     print urlmark1.select('a')[2].text#publisher
+#     print urlmark1.select('a')[1].text#title
+#     try:
+#         print urlmark1.select('a > span')[0].text#publish_time
+#     except Exception as e:
+#         print e
+#     print urlmark1.select('span')[1].text#publish_date
+#     # print urlmark1.select('div.news_li')[0].text
+#     # print urlmark1.select('')
+#     print urlmark1.select('h2 > a')[0].get('id')
+#     # print urlmark1.select('span')[].text
+#     try:
+#         reply_count= urlmark1.select('span.trbszan')[0].text
+#         if 'k' in reply_count:
+#             reply_count = reply_count.replace('.', '').replace('k', '00')
+#         print reply_count
+#     except Exception as e:
+#         print e
+#         print 0\\
+
+
+for div_content in datasoup.select('body > div'):
+    try:
+        print 'http://m.thepaper.cn/'+div_content.select('div > a')[0].get('href')#url
+        publish_time= div_content.select('p > span')[0].text#publish_time
+        print div_content.select('div > p > a')[1].text#title
+        print div_content.select('div > p > a')[0].text#publish_user
+        # print div_content
+        # if u'分钟' in publish_time:
+        if
+
+        print '\n\n\n'
+    except Exception as e:
+        print e
