1bb30054b7c9376c4f344e1836ad29584ce82a56
机房ｉｄｃ推送 <passager@163.com>
2017-09-12 14:42:40 +0800

    9-8-远程主机推送

--- spiders/chengshiluntan.py
+++ spiders/chengshiluntan.py
@@ -29,6 +29,7 @@ from KafkaConnector import RemoteProducer,Consumer
 
 
 class chengshiluntan:
+
     def __init__(self):
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
